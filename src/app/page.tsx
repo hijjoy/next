c1f277a62a06fef2bcb2daf0ad63968c3aedc962
hijjoy <hijjoy@naver.com>
2025-07-07 22:16:36 +0900

    feat: Lps 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
       <h1 className="text-4xl">크래빗 new 아키텍쳐 테스트</h1>
 
       <div className="flex items-center gap-5">
-        <Link href="/profile">Profile 아직 없음</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/lps">Lps</Link>
         <Link href="/login">login</Link>
       </div>
     </div>
--- src/features/users/presentation/ui/views/profile-view.tsx
+++ src/features/users/presentation/ui/views/profile-view.tsx
@@ -2,6 +2,7 @@ import getQueryClient from "@/shared/configs/tanstack-query/get-query-client";
 import ProfileSection from "../sections/profile-section";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getSSRContext } from "@/shared/context/ssr-context";
+import Link from "next/link";
 
 export default async function ProfileView() {
   const queryClient = getQueryClient();
@@ -19,6 +20,7 @@ export default async function ProfileView() {
       <HydrationBoundary state={dehydratedState}>
         <ProfileSection />
       </HydrationBoundary>
+      <Link href="/lps">Lps</Link>
     </div>
   );
 }
